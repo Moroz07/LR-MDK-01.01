d8eae8dfb462b1fc0fac1a93ed973cd60431e4f9
YanaTyzik <denisyuk_yanina@mail.ru>
2025-09-20 11:54:56 +0300

    Создали пустой текстовый список и заполнили его

---
+++ TestList/TestList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Text = new List<string>();
+            Text.Add("CASE");
+            Text.Add("Battle");
+            Text.Add("Top");
+            Text.Add("Do dep");
+            Text.Add("Nikita");
+        }
+    }
+}
