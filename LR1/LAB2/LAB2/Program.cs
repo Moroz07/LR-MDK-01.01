2221cc248ff5c3c02f5f27a8ecb4726fc9e0ce8b
Vladimir420911 <dhbtgvhb@gmail.com>
2025-09-25 09:20:59 +0300

    Создал числовой массив и заполнил его рандомными числами от 1 до 200

---
+++ LR1/LAB2/LAB2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[1500];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                array[i] = rand.Next(1, 200);
+            }
+
+            //foreach(int element in array)
+            //{
+            //    Console.Write(element + " ");
+            //}
+        }
+    }
+}
