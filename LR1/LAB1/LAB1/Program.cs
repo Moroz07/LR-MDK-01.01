55ac71a78838bdf76cd9335af3876fe2ddfff5d0
Vladimir420911 <dhbtgvhb@gmail.com>
2025-09-25 08:32:26 +0300

    Создал численный массив, у которого вводится значение и его размер с консоли

---
+++ LR1/LAB1/LAB1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int Length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[Length];
+            for(int i = 0; i< Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            
+            
+
+        }
+    }
+}
