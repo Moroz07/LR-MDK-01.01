9d3120440d4e9c3b8fdab56baf7fbec12a418ea5
sellasouls666 <pgbc303@gmail.com>
2025-09-27 11:46:05 +0300

    Добавили новый файл .c с названием ListStringsModule

---
+++ Fuctions/Fuctions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
