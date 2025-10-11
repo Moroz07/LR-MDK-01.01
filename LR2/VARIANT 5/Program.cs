c757fb1b0b1885383cd57541f59d8dd6b7d43813
st50 <zahar@gamil.com>
2025-10-11 12:54:53 +0300

    Реализовал использование функций в Program.cs, требуют выбрать жанр игры, после чего выводят их в порядке убывания

--- LR2/VARIANT 5/Program.cs
+++ LR2/VARIANT 5/Program.cs
@@ -8,8 +8,35 @@ namespace VARIANT_5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Print(List<string> games)
         {
+            Console.WriteLine(String.Join(", ", games));
+        }
+
+        static void Prints(List<int> count)
+        {
+            Console.WriteLine(String.Join(", ", count));
+        }
+
+        static void Main()
+        {
+            string[] genres = new string[] { "Экшн", "Стратегия", "Шутер" };
+            var (games, counts) = InputModule.InputGamesByCenre();
+
+            string userQuery = InputModule.InputUserQuery();
+
+            int indexGenre = SearchingModule.FindIndexGenre(userQuery, genres);
+            if (indexGenre < 0)
+            {
+                Console.WriteLine("Указан несуществующий жанр");
+                return;
+            }
+
+            var (gamesUserGenre, countsUserGenre) = SearchingModule.FindAllGamesByCenre(indexGenre, games, counts);
+
+            AnalysisDataModule.SortGames(gamesUserGenre, countsUserGenre);
+            Print(gamesUserGenre);
+            Prints(countsUserGenre);
         }
     }
 }
