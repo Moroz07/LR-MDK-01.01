014c5e7ffb60920acad7fbacd170ef5a7d97e445
Артём <morozov-f.morozov@yandex.ru>
2025-10-27 21:18:03 +0300

    Опрееделил функцию поиска индекса жанра

--- LR2/VARIANT 5/SearchingModule.cs
+++ LR2/VARIANT 5/SearchingModule.cs
@@ -10,31 +10,11 @@ namespace VARIANT_5
     {
         static public int FindIndexGenre(string userQuery, string[] genres)
         {
-            for (int index = 0; index < genres.Length; ++index)
-            {
-                string valueGenre = genres[index].ToLower();
-                if (valueGenre == userQuery.ToLower())
-                {
-                    return index;
-                }
-            }
-
-            return -1;
+           
         }
 
 
 
-        static public (List<string>, List<int>) FindAllGamesByCenre(int indexGenre,
-                                                                 List<string>[] allGames,
-                                                                 List<int>[] counts)
-        {
-            List<string> gamesByGenre = new List<string>();
-            List<int> countsByGenre = new List<int>();
-
-            gamesByGenre = allGames[indexGenre];
-            countsByGenre = counts[indexGenre];
-
-            return (gamesByGenre, countsByGenre);
-        }
+        
     }
 }
