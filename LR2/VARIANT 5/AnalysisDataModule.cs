6d83a55a314749f697acb392e4311609b8ebd79b
Артём <morozov-f.morozov@yandex.ru>
2025-10-27 21:26:28 +0300

    Реализовал функцию сортировки игр

--- LR2/VARIANT 5/AnalysisDataModule.cs
+++ LR2/VARIANT 5/AnalysisDataModule.cs
@@ -10,7 +10,23 @@ namespace VARIANT_5
     {
         static public void SortGames(List<string> games, List<int> counts)
         {
-            
+            for (int i = 0; i < counts.Count; ++i)
+            {
+                for (int j = 0; j < counts.Count - 1; ++j)
+                {
+                    bool condition = counts[j + 1] > counts[j];
+                    if (condition)
+                    {
+                        int temp_counts = counts[j];
+                        counts[j] = counts[j + 1];
+                        counts[j + 1] = temp_counts;
+
+                        string temp_books = games[j];
+                        games[j] = games[j + 1];
+                        games[j + 1] = temp_books;
+                    }
+                }
+            }
         }
 
         
