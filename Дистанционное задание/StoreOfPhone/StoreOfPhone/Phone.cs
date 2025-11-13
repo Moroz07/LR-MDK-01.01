7dffda47903adc4bf1a75df2b7d0fc48bb7d4535
sellasouls666 <pgbc303@gmail.com>
2025-11-13 10:22:09 +0300

    Создал класс Phone, в котором создал структуру Phone

---
+++ Дистанционное задание/StoreOfPhone/StoreOfPhone/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreOfPhone
+{
+
+    public struct Phone
+    {
+        public string name;
+        public int price;
+        public DateTime SellData;
+        public int count;
+    }
+}
+
