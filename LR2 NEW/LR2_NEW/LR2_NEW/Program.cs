e200d3948bdd384fa5c6e7fab15bf9d3aaaa3621
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-15 11:49:13 +0300

    Создал класс Struct, в котором описал стуктуру Phone

---
+++ LR2 NEW/LR2_NEW/LR2_NEW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2_NEW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ВАРИАНТ 12
+        }
+    }
+}
---
+++ LR2 NEW/LR2_NEW/LR2_NEW/Struct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2_NEW
+{
+    public struct Phone
+    {
+        public string brand;
+        public string model;
+        public int week;
+        public int countSell;
+    }
+}
