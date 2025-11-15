6f6e369630e881f29bbd63835fcbd8474bb844a7
Vladimir420911 <dhbtgvhb@gmail.com>
2025-11-15 13:08:20 +0300

    Cоздал функции, но не доделал, нужно сделать

--- LR2 NEW/LR2_NEW/LR2_NEW/Function.cs
+++ LR2 NEW/LR2_NEW/LR2_NEW/Function.cs
@@ -2,12 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LR2_NEW
 {
     class Function
     {
+        //public static double TopSellPhone(List<Phone> salesPhones)
+        //{
+        //    Console.WriteLine("Введите марку телефона и получите статистику(Samsung, Iphone, Xiaomi): ");
+        //    string Company = Console.ReadLine();
 
+        //        foreach (var phone in salesPhones)
+        //        {
+        //        if (phone.brand == Company)
+        //        {
+                    
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Такого бренда нету в магазине");
+        //            break;
+        //        }
+        //        }
+        //    return
+            
+        //}
+
+        public static List <double> AverageSale(List<Phone> phones)
+        {
+            List<double> average = new List<double>();
+            double count = 0;
+            foreach (var phone in phones)
+            {
+                for(int i=1; phone.week >= i; i++)
+                {
+                    count += phone.countSell;
+                }
+                average.Add(count);
+            }
+            List<double>  Copy = new List<double>(average);
+
+            for (int i = 0; i < Copy.Count - 1; i++)
+            {
+                for (int j = i + 1; j < Copy.Count; j++)
+                {
+                    if (Copy[j] > Copy[i])
+                    {
+                        double value = Copy[i];
+                        Copy[i] = Copy[j];
+                        Copy[j] = value;
+                    }
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Copy[i]}");
+                
+            }
+            return average;
+        }
     }
 }
--- LR2 NEW/LR2_NEW/LR2_NEW/Program.cs
+++ LR2 NEW/LR2_NEW/LR2_NEW/Program.cs
@@ -9,10 +9,44 @@ namespace LR2_NEW
 {
     class Program
     {
+
+        public static List<double> AverageSale(List<Phone> phones)
+        {
+            List<double> average = new List<double>();
+            double count = 0;
+            foreach (var phone in phones)
+            {
+                for (int i = 1; phone.week >= i; i++)
+                {
+                    count += phone.countSell;
+                }
+                average.Add(count);
+            }
+            List<double> Copy = new List<double>(average);
+
+            for (int i = 0; i < Copy.Count - 1; i++)
+            {
+                for (int j = i + 1; j < Copy.Count; j++)
+                {
+                    if (Copy[j] > Copy[i])
+                    {
+                        double value = Copy[i];
+                        Copy[i] = Copy[j];
+                        Copy[j] = value;
+                    }
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Copy[i]}");
+
+            }
+            return average;
+        }
         static void Main(string[] args)
         {
             // ВАРИАНТ 12
-
+            
             List<Phone> sales = new List<Phone>();
             sales.Add(new Phone { brand = "Samsung", model = "S25 ULTRA", week = 1, countSell = 1 });
             sales.Add(new Phone { brand = "Samsung", model = "S25 ULTRA", week = 2, countSell = 0 });
@@ -55,6 +89,8 @@ namespace LR2_NEW
             sales.Add(new Phone { brand = "Xiaomi", model = "POCO X7 PRO", week = 2, countSell = 1 });
             sales.Add(new Phone { brand = "Xiaomi", model = "POCO X7 PRO", week = 3, countSell = 2 });
             sales.Add(new Phone { brand = "Xiaomi", model = "POCO X7 PRO", week = 4, countSell = 3 });
+
+            AverageSale(sales);
 
 
         }
