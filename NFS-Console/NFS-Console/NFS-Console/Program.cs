5fb659fd85ff9f14cf26e1303289ad5aa23f7412
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-22 11:47:03 +0300

    Реализовал функцию гонки, и вызвыал её

--- NFS-Console/NFS-Console/NFS-Console/Program.cs
+++ NFS-Console/NFS-Console/NFS-Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NFS_Console
@@ -12,21 +13,66 @@ namespace NFS_Console
         {
             Console.WriteLine(car.GetModel());
         }
+
+        static public void Race(List <Car> cars)
+        {
+            int time = 0;
+            int finish = 80;
+            while (true)
+            {
+                
+                foreach (Car auto in cars)
+                {
+                    double speed = (auto.GetSpeed() / 50) * time;
+                    for (int i = 1; i <= speed; i++)
+                    {
+                        Console.Write(" ");
+                        if (speed >= finish)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Машина победитель: " + auto.GetModel());
+                            return;
+                        }
+                    }
+                    Console.WriteLine(auto.GetModel());
+                    Console.WriteLine("--------------------------------------------------------------------------------");
+
+                }
+                time++;
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
         static void Main(string[] args)
         {
             Car auto = new Car();
             auto.SetModel("Lada");
-            PrintModel(auto);
-            auto.SetSpeed(150);
+            auto.SetSpeed(1000);
 
             Car merc = new Car();
             merc.SetModel("Mercedes");
             merc.SetSpeed(300);
-            PrintModel(merc);
+
+            Car audi = new Car();
+            audi.SetModel("Audi");
+            audi.SetSpeed(250);
+
+            Car ford = new Car();
+            ford.SetModel("Ford");
+            ford.SetSpeed(350);
+
+            Car tesla = new Car();
+            tesla.SetModel("Tesla");
+            tesla.SetSpeed(275);
+
 
             List<Car> cars = new List<Car>();
             cars.Add(auto);
             cars.Add(merc);
+            cars.Add(audi);
+            cars.Add(ford);
+            cars.Add(tesla);
+            Race(cars);
         }
     }
 }
