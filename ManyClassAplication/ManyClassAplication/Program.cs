086264108c460c89bfe67c985284dbb55da35a58
Dizablee <mth78931@gmail.com>
2025-11-25 13:23:40 +0300

    Создал 2 класса, в котором хранятся данные о товарах и складах, заполнил некоторые данные и сейчас занимаюсь выводом машин и их количества

---
+++ ManyClassAplication/ManyClassAplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClassAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Goods car1 = new Goods();
+            car1.SetName("Mercedes-Benz");
+            car1.SetPrice(1200000);
+            Goods car2 = new Goods();
+            car2.SetName("УАЗ");
+            car2.SetPrice(10000000);
+
+            Warehouse storage = new Warehouse();
+            storage.SetIdentifier(1);
+            storage.SetLocation("Торжок, Студенческая 3.");
+            storage.SetProductCount(car1, 2);
+            storage.SetProductCount(car2, 5);
+
+           
+           Console.WriteLine("Адрес склада: " + storage.GetLocation());
+           Console.WriteLine("Идентификатор склада: " + storage.GetIdentifier());
+           foreach(Goods car in storage.GetProductCount().Keys)
+           {
+                
+                Console.WriteLine();
+           }    
+            
+        }
+    }
+}
---
+++ ManyClassAplication/ManyClassAplication/Warehouse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyClassAplication
+{
+    class Warehouse
+    {
+        private int identifier_;
+        private string location_;
+        private Dictionary<Goods, int> productsCount_ = new Dictionary<Goods, int>();
+
+        public void SetIdentifier(int identifier)
+        {
+            identifier_ = identifier;
+        }
+
+        public int GetIdentifier()
+        {
+            return identifier_;
+        }
+
+        public void SetLocation(string location)
+        {
+            location_ = location;
+        }
+
+        public string GetLocation()
+        {
+            return location_;
+        }
+
+        public void SetProductCount(Goods product, int count)
+        {
+            productsCount_.Add(product, count);
+        }
+
+        public Dictionary<Goods, int> GetProductCount()
+        {
+            return productsCount_;
+        }
+    }
+}
