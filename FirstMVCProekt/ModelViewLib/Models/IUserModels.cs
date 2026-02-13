e329719d7b801d48c54ae8297412e7e881276c0e
Vladimir420911 <dhbtgvhb@gmail.com>
2026-02-13 09:07:34 +0300

    связал два прокта по ссылке, создал класс User и Интерфейс IUserModels

---
+++ FirstMVCProekt/ModelViewLib/Models/IUserModels.cs
@@ -0,0 +1,14 @@
+﻿using ModelViewLib.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewLib.Models
+{
+    public interface IUserModels
+    {
+        List<User> Load();
+    }
+}
