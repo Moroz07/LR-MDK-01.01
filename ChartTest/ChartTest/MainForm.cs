1a01bc5bc9e6cee3594159716f71368069936be2
Moroz <morozov-f.morozov@yandex.ru>
2026-03-10 11:08:25 +0300

    реализова вывод информации на PieChart

--- ChartTest/ChartTest/MainForm.cs
+++ ChartTest/ChartTest/MainForm.cs
@@ -1,5 +1,11 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
+using SalesLibrary;
+using SalesLibrary.Analysis;
+using SalesLibrary.Presenters;
+using SalesLibrary.Views;
+using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Windows.Media;
 
@@ -7,100 +13,71 @@ namespace ChartTest
 {
     public partial class MainForm : Form
     {
-
+        private SalesPresenter presenter_;
         void FillCartesianChart()
         {
-            LineSeries series = new LineSeries
+            ItemsList.DataSource = presenter_.GetAllItems();
+            ItemsList.DisplayMember = "Name";
+            if (ItemsList.Items.Count > 0)
             {
-                Title = "Продажи",
-                Values = new ChartValues<int> { 10, 15, 8, 19, 25, 22 },
-
-                Stroke = new SolidColorBrush(Colors.Gold),
-                StrokeThickness = 2,
-
-                PointGeometry = DefaultGeometries.Circle,
-                PointGeometrySize = 7,
-
-                Fill = new LinearGradientBrush(
-                    System.Windows.Media.Color.FromArgb(100, 153, 0, 18),
-                    System.Windows.Media.Color.FromArgb(100, 153, 0, 18),
-                    90)
-            };
-
-            cartesian.Series = new SeriesCollection { series, /*series_2, series_3*/ };
-
-            /// Ось Y
-            cartesian.AxisY.Add(new Axis
-            {
-                Foreground = System.Windows.Media.Brushes.Black,
-                LabelFormatter = value => value.ToString("NO"),
-
-                Separator = new Separator
-                {
-                    Stroke = new SolidColorBrush(Color.FromArgb(40, 0, 0, 0)),
-                    StrokeThickness = 1
-                },
-
-                MaxValue = 30,
-                MinValue = 1
+                presenter_.ShowSalesByItem(((Item)ItemsList.Items[0]).Name);
             }
-            );  
-
-
-            /// Ось X
-            cartesian.AxisX.Add(new Axis
-            {
-                Foreground = System.Windows.Media.Brushes.Black,
-                Labels = new[] { "Янв", "Фев", "Мар", "Апр", "Май", "Июн" },
-
-                Separator = new Separator
-                {
-                    IsEnabled = false,
-                },
-            }
-            );
         }
 
         void FillAngular()
         {
-            angular.Value = 65;
             angular.FromValue = 0;
             angular.ToValue = 100;
 
-            angular.TicksForeground = Brushes.Purple;
-            angular.NeedleFill = Brushes.Green;
-
-            
+            angular.TicksForeground = Brushes.Gray;
+            angular.NeedleFill = Brushes.DarkBlue;
         }
 
         void FillSolid()
         {
-            solid.Value = 65;
             solid.From = 0;
             solid.To = 100;
-            solid.Uses360Mode = true;
             solid.LabelFormatter = value => value + "%";
-            solid.GaugeBackground = Brushes.LawnGreen;
-            solid.Base.GaugeActiveFill = new System.Windows.Media.LinearGradientBrush
-            {
-                GradientStops = new System.Windows.Media.GradientStopCollection
-                {
-                new System.Windows.Media.GradientStop(System.Windows.Media.Colors.Yellow,.5),
-                new System.Windows.Media.GradientStop(System.Windows.Media.Colors.Red,0),
-                new System.Windows.Media.GradientStop (System.Windows.Media.Colors.Purple,1)
-                }
-            };
-            
+        }
+
+        void FillPieChart()
+        {
+            piechart.SetPresenter(presenter_);
+            piechart.UpdateView();
+            piechart.InnerRadius = 30;
+
         }
         public MainForm()
         {
             InitializeComponent();
+
+            presenter_ = new SalesPresenter(new List<ISalesView> { cartesian });
 
             FillCartesianChart();
 
             FillAngular();
 
             FillSolid();
+
+            FillPieChart();
+        }
+
+        private void ItemsList_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            Item selectedItem = ((Item)(ItemsList.SelectedItem));
+            if(selectedItem == null)
+            {
+                return;
+            }
+
+            presenter_.ShowSalesByItem(selectedItem.Name);
+            double percent = Math.Round(
+                presenter_.GetProfitPercentByItem(selectedItem), 2);
+
+            angular.Value = percent;
+            solid.Value = percent;
+
+            
         }
     }
 }
---
+++ ChartTest/ChartTest/Views/SalesPieChart.cs
@@ -0,0 +1,44 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using SalesLibrary;
+using SalesLibrary.Models;
+using SalesLibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SalesLibrary.Analysis;
+using SalesLibrary.Presenters;
+
+namespace ChartTest.Views
+{
+    class SalesPieChart : LiveCharts.WinForms.PieChart
+                                    
+    {
+        private SalesPresenter presenter_;
+
+        public void SetPresenter(SalesPresenter presenter)
+        {
+            presenter_ = presenter;
+        }
+        public void UpdateView()
+        {
+            List<Item> allItems = presenter_.GetAllItems();
+            SeriesCollection seriesData_ = new SeriesCollection();
+            foreach (Item item in allItems)
+            {
+                seriesData_.Add(new PieSeries
+                {
+                    Title = item.Name,
+                    Values = new ChartValues<double> { presenter_.GetProfitPercentByItem(item)},
+                    
+                });
+            }
+            Series = seriesData_;
+            LegendLocation = LegendLocation.Right;
+        }
+    }
+}
