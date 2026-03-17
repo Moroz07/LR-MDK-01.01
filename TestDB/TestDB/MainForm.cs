1f6f73a663ba582090535a198d94a23a3f385518
Moroz07 <morozov-f.morozov@yandex.ru>
2026-03-17 11:12:53 +0300

    Сделал программу, по тестовому подключению БД

---
+++ TestDB/TestDB/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace TestDB
+{
+    public partial class MainForm: Form
+    {
+        PGUsersLoader postgres = new PGUsersLoader();
+        public MainForm()
+        {
+            InitializeComponent();
+            UsersDataGridView.DataSource = postgres.Load();
+            
+        }
+    }
+}
---
+++ TestDB/TestDB/PGUsersLoader.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDB
+{
+    class PGUsersLoader
+    {
+        public List<User> Load()
+        {
+            List<User> allUsers = new List<User>();
+            var cs = "Host=192.168.1.48;Username=st50-5;Password=505;Database=ProbTestt";
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT login,password,name,lastname,age FROM myusers";
+
+            var cmd = new NpgsqlCommand(sql, con);
+
+
+            var reader = cmd.ExecuteReader();
+
+            List<string> alllogin = new List<string>();
+
+            while (reader.Read())
+            {
+                allUsers.Add(new User { Login = reader.GetString(0),
+                    Password = reader.GetString(1),
+                    Name = reader.GetString(2), 
+                    LastName = reader.GetString(3),
+                    Age = reader.GetInt32(4) });
+
+            }
+            con.Close();
+            return allUsers;
+        }
+    }
+}
